39436f7cad8dfbbb7c64b81bea4c3bd28f788451
leonardo-lerer <81173712+leonardo-lerer@users.noreply.github.com>
2023-05-16 23:56:10 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,8 +1,18 @@
 from PIL import Image
 from io import BytesIO
-
+import numpy as np
+import pickle
 import base64
 
 def fun_open(data):
   data = data[22:] + "===="
-  return Image.open(BytesIO(base64.b64decode(data)))
+  im = Image.open(BytesIO(base64.b64decode(data)))
+  im = np.array(im.resize((128,128)))
+  im = x[np.newaxis,...]
+  im = np.reshape(conv_model(x), (1,-1))
+  conv_model = pickle.load(open("/static/conv_model.pkl, "rb"))
+  xgb_model = pickle.load(open("/static/xgb_model.pkl, "rb"))
+  res = xgb_model.predict_proba(x)
+  return "try"
+  
+  
