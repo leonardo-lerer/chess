39ca2195702ea95234c358ba99da468499a6351e
leonardo-lerer <81173712+leonardo-lerer@users.noreply.github.com>
2023-05-16 10:30:28 +0300

    Update server.py

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-  return render_template("index2.html")
+  return render_template("index.html")
 
 @app.route("/upload", methods=['GET','POST'])
 def upload():
