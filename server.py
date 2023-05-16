4e731d630b71ba1945b2cd702f5f08277acb3e34
leonardo-lerer <81173712+leonardo-lerer@users.noreply.github.com>
2023-05-16 10:16:42 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,25 @@
 
+from flask import Flask
+from flask import render_template
+from flask import request
+
+import model
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+  return render_template("index2.html")
+
+@app.route("/upload", methods=['GET','POST'])
+def upload():
+  params = request.get_json(force=True)
+  im = params["img"]
+  im = model.fun_open(im)
+  
+  
+  return "try"
+  
+if __name__ == "__main__":
+  app.run(debug=True)
