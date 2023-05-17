7f4d086f05c2592d93ec7b46c7c490c1d6ff3bec
leonardo-lerer <81173712+leonardo-lerer@users.noreply.github.com>
2023-05-17 15:58:13 +0300

    Update model.py

--- model.py
+++ model.py
@@ -9,15 +9,14 @@ import lightgbm
 int_to_str = {1: "wr", 2: "wn", 3: "wb", 4 : "wq", 5 : "wk", 6 : "wp",
               7 : "br", 8 : "bn", 9 : "bb", 10 : "bq", 11 : "bk", 12 : "bp",
               0 : "nn"}
+conv_model = tf.keras.models.load_model('static/conv_model.h5')
+gbm_model = pickle.load(open("static/gbm_model.pkl", "rb"))
 
 def fun_open(data):
   data = data[22:] + "===="
   im = Image.open(BytesIO(base64.b64decode(data))).convert('RGB')
   im = np.array(im.resize((128,128)))
   im = im[np.newaxis,...]
-  
-  conv_model = tf.keras.models.load_model('static/conv_model.h5')
-  gbm_model = pickle.load(open("static/gbm_model.pkl", "rb"))
   im = tf.reshape(conv_model(im), [1,-1])
   res = int_to_str[np.argmax(gbm_model.predict(im))]
 #   res = np.array2string(gbm_model.predict(im), precision=3, separator=',')
